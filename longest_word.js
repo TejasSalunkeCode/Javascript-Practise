8946072549b76f20d45313d4e6949b1a2b891fb2
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-03-08 18:25:09 +0530

    findLongestWord

---
+++ longest_word.js
@@ -0,0 +1,15 @@
+const findLongestWord = (str) => {
+  if (str.trim().length === 0) {
+    return false;
+  }
+  words = str.split(" ");
+  // words = words.sort((a, b) => a.length - b.length);
+  // return words.at(-1);
+
+  return words.reduce(
+    (accum,curWord)=>(curWord.length>accum.length ? curWord : accum)," ")
+};
+
+console.log(
+  findLongestWord("Watch Thapa Technical javascript course on youtube"),
+);
