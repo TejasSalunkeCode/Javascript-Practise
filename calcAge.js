fea37212786b8afbb18c800eae6cd2069c55918d
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-03-11 10:33:28 +0530

    refactor calcAge function to correctly calculate age based on date of birth

--- calcAge.js
+++ calcAge.js
@@ -2,6 +2,12 @@ const calcAge=(d1)=>{
     const dob = new Date(d1);
     const date= new Date();
     console.log(date);
-    return date-dob/24*60;
+    let age=date.getFullYear()-dob.getFullYear();
+    // console.log(age);
+    let monthDiff=date.getMonth()-dob.getMonth();
+    if(monthDiff<0 || (monthDiff===0 && date.getDate()<dob.getDate())){
+        age--;
+    }
+    return age;
 }
-console.log(calcAge("1990-05-15"));
+console.log(calcAge("2005-11-13"));
